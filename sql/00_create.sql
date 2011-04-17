ee450dd15c95050eb55c790cce6ae61726ec5f4b
Ondrej Mikle <ondrej.mikle@gmail.com>
2011-04-17 22:37:52 +0200

    updated SQL scheme to make it close to original sqlite scheme

--- sql/00_create.sql
+++ sql/00_create.sql
@@ -1,23 +1,38 @@
-DROP VIEW IF EXISTS observations_hex;
+DROP VIEW IF EXISTS observations_view;
 DROP TABLE IF EXISTS observations;
-DROP TYPE IF EXISTS service_type;
+DROP TABLE IF EXISTS service;
 
-CREATE TYPE service_type AS ENUM ('ssh', 'ssl');
-
--- id not needed yet, later may be used for foreign keys
-CREATE TABLE observations (
-    id serial UNIQUE NOT NULL,
-    fqdn VARCHAR(255) NOT NULL,
-    port integer NOT NULL,
-    service service_type NOT NULL,
-    md5 bytea NOT NULL,
-    sha1 bytea,
-
-    PRIMARY KEY (fqdn, port, service)
+-- This would be better as enum, but then we'd have to rewrite a lot of
+-- original code.
+CREATE TABLE service (
+    id SMALLINT UNIQUE NOT NULL,
+    name VARCHAR(16) NOT NULL
 );
 
-CREATE VIEW observations_hex AS
-    SELECT id, fqdn, port, service, 
+INSERT INTO service (id, name) VALUES 
+    (1, 'ssh'),
+    (2, 'ssl');
+
+CREATE TABLE observations (
+    id serial PRIMARY KEY,
+    fqdn VARCHAR(255) NOT NULL,
+    port integer NOT NULL,
+    service_type SMALLINT REFERENCES service(id),
+    start_time timestamp with time zone NOT NULL,
+    end_time timestamp with time zone NOT NULL,
+    md5 bytea NOT NULL,
+    sha1 bytea -- allowed null to allow old DB migration
+);
+
+-- (fqdn, port, service_type) is not unique since fingerprints may change
+CREATE INDEX service_idx ON observations (fqdn, port, service_type);
+
+CREATE VIEW observations_view AS
+    SELECT id, fqdn, port, service_type,
+        date_part('epoch', start_time)::int AS start_ts,
+        date_part('epoch', end_time)::int AS end_ts,
+        md5,
         encode(md5, 'hex') AS md5hex, 
-        encode(sha1, 'hex') AS sha1hex 
+        sha1,
+        encode(sha1, 'hex') AS sha1hex
         FROM observations;
