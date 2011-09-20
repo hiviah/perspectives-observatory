c193212c5853ccdd6604d1f9e7c219ebd4b5f7be
Ondrej Mikle <ondrej.mikle@nic.cz>
2011-09-20 15:56:27 +0200

    New DB schema for storing end-entity certs as well as CA certs in chains

--- sql/00_create.sql
+++ sql/00_create.sql
@@ -1,10 +1,13 @@
 DROP VIEW IF EXISTS observations_view;
-DROP TABLE IF EXISTS observations;
+DROP TABLE IF EXISTS ee_certs_x_ca_certs;
+DROP TABLE IF EXISTS ca_certs;
+DROP TABLE IF EXISTS ee_certs;
 DROP TYPE IF EXISTS service_type_enum;
 DROP TABLE IF EXISTS services;
 
 CREATE TYPE service_type_enum AS ENUM ('ssl', 'ssh');
 
+-- All observed hosts
 CREATE TABLE services (
     id serial PRIMARY KEY,
     host VARCHAR(255) NOT NULL,
@@ -12,28 +15,47 @@ CREATE TABLE services (
     service_type service_type_enum NOT NULL DEFAULT 'ssl'
 );
 
-CREATE TABLE observations (
+-- Table with CA/intermediate certificates
+CREATE TABLE ca_certs (
+    id serial PRIMARY KEY,
+    certificate bytea NOT NULL
+);
+
+-- Table with end-entity, or server-certificates
+CREATE TABLE ee_certs (
     id serial PRIMARY KEY,
     start_time timestamp with time zone NOT NULL,
     end_time timestamp with time zone NOT NULL,
     certificate bytea NOT NULL,
-    md5 bytea NOT NULL,
-    sha1 bytea NOT NULL,
     service_id INTEGER NOT NULL REFERENCES services(id)
+);
+
+-- Defines certificate chains; order of chains is given by id of (ee_cert_id,
+-- ca_cert_id) pairs. Higher id means closer to chain root.
+CREATE TABLE ee_certs_x_ca_certs (
+    id serial PRIMARY KEY,
+    ee_cert_id INTEGER NOT NULL REFERENCES ee_certs(id),
+    ca_cert_id INTEGER NOT NULL REFERENCES ca_certs(id)
 );
 
 CREATE UNIQUE INDEX service_idx ON services (host, port, service_type);
 
-CREATE INDEX observations_fkey_service_idx ON observations (service_id);
-CREATE INDEX end_time_idx ON observations (end_time);
+CREATE INDEX ee_certs_fkey_service_idx ON ee_certs (service_id);
+CREATE INDEX ee_certs_end_time_idx ON ee_certs (end_time);
+CREATE INDEX ee_certs_x_ca_certs_service_id_idx ON ee_certs_x_ca_certs (ee_cert_id);
 
--- CREATE VIEW observations_view AS
---     SELECT id, host, port, service_type,
---         date_part('epoch', start_time)::int AS start_ts,
---         date_part('epoch', end_time)::int AS end_ts,
---         md5,
---         encode(md5, 'hex') AS md5_hex, 
---         sha1,
---         encode(sha1, 'hex') AS sha1_hex
---         FROM observations;
+-- Creating index on md5() is for index speedup, collision (non)-resistance is
+-- not an issue.
+CREATE INDEX ca_certs_cert_idx ON ca_certs(md5(certificate));
+
+-- View for querying results for Perspectives HTTP API.
+CREATE VIEW observations_view AS
+    SELECT ee.id AS id, 
+        s.host AS host, 
+        s.port AS port, 
+        s.service_type AS service_type,
+        date_part('epoch', ee.start_time)::int AS start_ts,
+        date_part('epoch', ee.end_time)::int AS end_ts,
+        ee.certificate AS certificate
+    FROM services AS s INNER JOIN ee_certs AS ee ON (ee.service_id = s.id);
 
