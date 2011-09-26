9414939c91917438c3e8faa0c37eecc217b7d24c
Ondrej Mikle <ondrej.mikle@nic.cz>
2011-09-26 11:19:47 +0200

    Changed semantics of end_time timestamp in ee_certs: No longer updating end_time for most recent other cert when storing new cert.

--- notary_common.py
+++ notary_common.py
@@ -353,15 +353,14 @@ def report_observation(service_id, observation, timestamp=None):
 				observation.ee_cert(), update_time)
 		
 		if updated_count < 1:
-			#update most recent older cert for the service_id to make it seem
-			#"continuous" when viewed by perspectives SVG viewer in Firefox,
-			#which mostly makes sense in case of normal (non-CDN) cert rollover
-			(recent_ee_cert_id, _) = get_most_recent_ee_cert(service_id)
-			if recent_ee_cert_id:
-				update_ee_cert_timestamp_by_id(recent_ee_cert_id, update_time)
+			#We don't update end_time timestamp of the other last
+			#seen cert for this service like old Perspectives did.
+			#The semantics of end_time should be when the cert was
+			#actually last seen, not to attempt having "continuous"
+			#timeline in the SVG viewer.
 			
-			# cert has changed or no observations exist yet for this service_id.  Either way
-			# add a new entry for this key with timespan start and end set to the current time
+			# Observation for service with this cert not seen yet,
+			# insert new observation.
 			db_service_id = store_service_id(service_id)
 			ee_cert_id = store_ee_cert(observation, db_service_id, update_time)
 			ca_cert_ids = store_ca_chain_certs(observation)
